ed339aca2f85eedac9b8447a82a39d22e4966d17
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-01-07 16:57:21 +0300

    Uploaded setup.py (0.2b)

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='DPULSE',
+    version='HEARTBEAT',
+    description='Domain Public Data Collection Service',
+    author_email='osint.technologies@gmail.com',
+    install_requires=[
+        'Jinja2 == 3.1.2',
+        'MarkupSafe == 2.1.3',
+        'beautifulsoup4 == 4.12.2',
+        'bs4 == 0.0.1',
+        'certifi == 2023.11.17',
+        'charset-normalizer == 3.3.2',
+        'fake-useragent == 1.2.1',
+        'idna == 3.6',
+        'pdfkit == 1.0.0',
+        'requests == 2.31.0',
+        'soupsieve == 2.5',
+        'urllib3 == 2.1.0',
+        'useragent == 0.1.1',
+        'whois == 0.9.27',
+        'colorama == 0.4.6',
+        'pyfiglet == 1.0.2',
+        'rich == 13.5.2'
+    ],
+)
