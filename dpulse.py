4d94a25ca47e41354cdda9569c47c5ebb1f1a459
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-01-07 16:55:55 +0300

    Updated dpulse.py to 0.2b

--- dpulse.py
+++ dpulse.py
@@ -1,33 +1,56 @@
 """
 Program start point
 
-You can call this script from yours system terminal: python main.py -u "url" -ra "number"
+You can call this script from yours system terminal: python dpulse.py
 
 """
 
 import report_creation as rc
-import argparse
+import itertools
+import time
+import threading
+from colorama import Fore, Style, Back
+from pyfiglet import Figlet
+from rich.console import Console
+class ProgressBar(threading.Thread):
+    def __init__(self):
+        super(ProgressBar, self).__init__()
+        self.do_run = True
 
-attributes_parser = argparse.ArgumentParser(description='OSINT methodology tool')
+    def run(self):
+        for char in itertools.cycle('|/-\\'):
+            if not self.do_run:
+                break
+            print(Fore.LIGHTMAGENTA_EX + Back.WHITE + char + Style.RESET_ALL, end='\r')
+            time.sleep(0.1)
 
-attributes_parser.add_argument('-sd',
-                       '--url',
-                       action='store',
-                       type=str,
-                       required=True,
-                       help='Attribute which contains website to research and investigate. Should be shorted: google.com, github.com')
+console = Console()
+fig = Figlet(font='univers')
+console.print(fig.renderText('DPULSE'), style="bold blue")
+print(Fore.BLUE + Back.WHITE + 'HEARTBEAT // version: 0.2b' + Style.RESET_ALL)
+print(Fore.BLUE + Back.WHITE + 'Developed by: OSINT-TECHNOLOGIES (https://github.com/OSINT-TECHNOLOGIES)' + Style.RESET_ALL + '\n\n')
 
-attributes_parser.add_argument('-ra',
-                       '--ra',
-                       action='store',
-                       type=int,
-                       required=True,
-                       help='Attribute which specify output amount of Google Dorking.')
+def print_menu():
+    print(Fore.GREEN + "1. Determine target and start scan")
+    print(Fore.RED + "2. Exit DPULSE" + Style.RESET_ALL + '\n')
 
-args = attributes_parser.parse_args()
-
-print(f"Processing scan of {args.url}")
-
-url = "http://" + str(args.url) + "/"
-
-rc.create_report(str(args.url), url, args.ra)
+while True:
+    print_menu()
+    choice = input(Fore.YELLOW + "Enter your choice >> ")
+    if choice == "1":
+        short_domain = str(input(Fore.YELLOW + "Enter target's short domain >> "))
+        url = "http://" + short_domain + "/"
+        dorking_results_amount = int(input(Fore.YELLOW + 'Enter amount of printed Google Dorking results >> '))
+        print(Fore.GREEN + 'Determined target >> {}\nShow {} Google Dorking result'.format(short_domain, dorking_results_amount) + Style.RESET_ALL)
+        spinner_thread = ProgressBar()
+        spinner_thread.start()
+        try:
+            rc.create_report(short_domain, url, dorking_results_amount)
+        finally:
+            spinner_thread.do_run = False
+            spinner_thread.join()
+    elif choice == "2":
+        print(Fore.RED + "Exiting the program." + Style.RESET_ALL)
+        break
+    else:
+        print(Fore.RED + "Invalid choice. Please enter an existing menu item" + Style.RESET_ALL)
