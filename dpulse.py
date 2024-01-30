a9ea5c5d90b79f7979a4f304749d40bfc51c2db4
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-01-30 21:38:33 +0300

    Updated dpulse.py to 0.3b

--- dpulse.py
+++ dpulse.py
@@ -2,16 +2,21 @@
 Program start point
 
 You can call this script from yours system terminal: python dpulse.py
-
 """
 
 import report_creation as rc
-import itertools
-import time
-import threading
-from colorama import Fore, Style, Back
-from pyfiglet import Figlet
-from rich.console import Console
+
+try:
+    import itertools
+    import time
+    import threading
+    from colorama import Fore, Style, Back
+    from pyfiglet import Figlet
+    from rich.console import Console
+    import sys
+except ImportError:
+    print(Fore.RED + "Can't import some requirements that are necessary to start DPULSE. Please check that all necessary requirements are installed!" + Style.RESET_ALL)
+    sys.exit()
 class ProgressBar(threading.Thread):
     def __init__(self):
         super(ProgressBar, self).__init__()
@@ -27,15 +32,52 @@ class ProgressBar(threading.Thread):
 console = Console()
 fig = Figlet(font='univers')
 console.print(fig.renderText('DPULSE'), style="bold blue")
-print(Fore.BLUE + Back.WHITE + 'HEARTBEAT // version: 0.2b' + Style.RESET_ALL)
+print(Fore.BLUE + Back.WHITE + 'HEARTBEAT // version: 0.3b' + Style.RESET_ALL)
 print(Fore.BLUE + Back.WHITE + 'Developed by: OSINT-TECHNOLOGIES (https://github.com/OSINT-TECHNOLOGIES)' + Style.RESET_ALL + '\n\n')
 
-def print_menu():
+def print_main_menu():
+    print('\n')
+    print(Fore.BLUE + '[MAIN MENU]')
     print(Fore.GREEN + "1. Determine target and start scan")
-    print(Fore.RED + "2. Exit DPULSE" + Style.RESET_ALL + '\n')
+    print(Fore.GREEN + "2. Settings")
+    print(Fore.RED + "3. Exit DPULSE" + Style.RESET_ALL)
+
+def print_settings_menu():
+    print('\n')
+    print(Fore.BLUE + '[SETTINGS MENU]')
+    print(Fore.GREEN + "1. Show current config")
+    print(Fore.GREEN + "2. Edit config parameters")
+    print(Fore.RED + "3. Return to main menu" + Style.RESET_ALL + '\n')
+
+def print_cfg_edit_menu():
+    print(Fore.BLUE + '[SETTINGS EDITING]')
+    print(Fore.GREEN + "1. Change sleep-interval")
+    print(Fore.GREEN + "2. Change timeout")
+    print(Fore.GREEN + "3. Add Dorking query to the list")
+    print(Fore.GREEN + "4. Remove Dorking query from the list" + Style.RESET_ALL + '\n')
+
+def change_setting(filename):
+    cfg_context = open(filename).read()
+
+    print('\n~ START OF CONFIG FILE ~')
+    print('\n' + Fore.BLUE + cfg_context + Style.RESET_ALL)
+    print(Fore.YELLOW + '\n~ END OF CONFIG FILE ~\n')
+
+    setting = input('Enter setting to change >> ')
+    new_value = input('Enter new value >> ')
+
+    with open(filename, 'r+') as file:
+        lines = file.readlines()
+        for i, line in enumerate(lines):
+            if line.startswith(setting + ':'):
+                lines[i] = f'{setting}: {new_value}\n'
+        file.seek(0)
+        file.writelines(lines)
+        print('\n')
+        print(Fore.GREEN + 'Setting {} successfully changed to {}'.format(setting, new_value))
 
 while True:
-    print_menu()
+    print_main_menu()
     choice = input(Fore.YELLOW + "Enter your choice >> ")
     if choice == "1":
         short_domain = str(input(Fore.YELLOW + "Enter target's short domain >> "))
@@ -50,6 +92,21 @@ while True:
             spinner_thread.do_run = False
             spinner_thread.join()
     elif choice == "2":
+        print_settings_menu()
+        choice_settings = input(Fore.YELLOW + "Enter your choice >> ")
+        if choice_settings == '1':
+            with open('config.txt', 'r') as cfg_file:
+                print('\n~ START OF CONFIG FILE ~')
+                print('\n' + Fore.BLUE + cfg_file.read() + Style.RESET_ALL)
+                print(Fore.YELLOW + '\n~ END OF CONFIG FILE ~\n')
+                continue
+        elif choice_settings == '2':
+            change_setting('config.txt')
+            continue
+        elif choice_settings == '3':
+            continue
+        break
+    elif choice == "3":
         print(Fore.RED + "Exiting the program." + Style.RESET_ALL)
         break
     else:
