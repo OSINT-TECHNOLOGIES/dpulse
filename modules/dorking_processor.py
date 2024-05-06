bf856ea9fce6bc61c0cbfaee728db689331ee304
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-05-06 23:33:45 +0300

    Updated dorking_processor.py to 0.8b

--- modules/dorking_processor.py
+++ modules/dorking_processor.py
@@ -1,13 +1,11 @@
-from googlesearch import search
-from time import sleep
+import requests.exceptions
 from colorama import Fore, Style
-import get_proxies as gp
-import nest_asyncio
-import asyncio
-import random
+import mechanicalsoup
+import re
+import requests
 
-nest_asyncio.apply()
-def get_query(short_domain):
+def get_dorking_query(short_domain):
+    print(Fore.GREEN + "Getting dorking query from config file")
     with open('config.txt', 'r') as cfg_file:
         lines = cfg_file.readlines()
         index = lines.index('[SOLID DORKS]\n')
@@ -15,34 +13,42 @@ def get_query(short_domain):
         search_query = [line.format(short_domain) for line in lines_after]
         return search_query
 
-def select_random_proxy(proxies_list):
-    selected_proxy = random.choice(proxies_list)
-    proxies_list.remove(selected_proxy)
-    return selected_proxy
-def solid_google_dorking(short_domain, sleep_interval, dorking_results_amount):
-    proxies_list = asyncio.run(gp.get_proxies())
-    search_delay = 5
-    print(Fore.GREEN + "Sleep interval set to {} seconds".format(sleep_interval))
-    print(Fore.GREEN + "Processing Google Dorking" + Style.RESET_ALL)
-    exposed_documents_query = str(get_query(short_domain)[0])
-    linkedin_query = str(get_query(short_domain)[1])
-    exposed_databases_query = str(get_query(short_domain)[2])
+def solid_google_dorking(query, pages=10):
     try:
-        proxy = select_random_proxy(proxies_list)
-        print(Fore.GREEN + "Dorking {} with {} proxy".format(exposed_documents_query, proxy))
-        exposed_documents = list(search(exposed_documents_query, sleep_interval=sleep_interval, num_results=dorking_results_amount, proxy=proxy))
-        print(exposed_documents)
-        sleep(search_delay)
-        print(Fore.GREEN + "Initiated {} seconds delay. Please, wait".format(search_delay) + Style.RESET_ALL)
-        proxy = select_random_proxy(proxies_list)
-        print(Fore.GREEN + "Dorking {} with {} proxy".format(exposed_documents_query, proxy))
-        linkedin_links = list(search(linkedin_query, sleep_interval=sleep_interval, num_results=dorking_results_amount))
-        sleep(search_delay)
-        print(Fore.GREEN + "Initiated {} seconds delay. Please, wait".format(search_delay) + Style.RESET_ALL)
-        proxy = select_random_proxy(proxies_list)
-        print(Fore.GREEN + "Dorking {} with {} proxy".format(exposed_documents_query, proxy))
-        exposed_databases = list(search(exposed_databases_query, sleep_interval=sleep_interval, num_results=dorking_results_amount))
-    except Exception:
-        print(Fore.RED + "[Error #004] - Request error. Error while processing Google Dorking, possibly caused by 429 Client Error" + Style.RESET_ALL)
+        browser = mechanicalsoup.StatefulBrowser()
+        browser.open("https://www.google.com/")
+        browser.select_form('form[action="/search"]')
+        browser["q"] = str(query)
+        browser.submit_selected(btnName="btnG")
+        result_query = []
+        for page in range(pages):
+            for link in browser.links():
+                target = link.attrs['href']
+                if (target.startswith('/url?') and not
+                target.startswith("/url?q=http://webcache.googleusercontent.com")):
+                    target = re.sub(r"^/url\?q=([^&]*)&.*", r"\1", target)
+                    result_query.append(target)
+            try:
+                browser.follow_link(nr=page + 1)
+            except mechanicalsoup.LinkNotFoundError:
+                break
+        del result_query[-2:]
+        return result_query
+    except requests.exceptions.ConnectionError:
+        print(Fore.RED + "Error while establishing connection with domain. No results will appear" + Style.RESET_ALL)
 
-    return exposed_documents, linkedin_links, exposed_databases
+def save_results_to_txt(folderpath, queries, pages=10):
+    txt_writepath = folderpath + '//04-dorking_results.txt'
+    with open(txt_writepath, 'w') as f:
+        for i, query in enumerate(queries, start=1):
+            f.write(f"QUERY #{i}: {query}\n")
+            results = solid_google_dorking(query, pages)
+            if not results:
+                f.write("=> NO RESULT FOUND\n")
+            else:
+                for result in results:
+                    f.write(f"=> {result}\n")
+            f.write("\n")
+    print(Fore.GREEN + "Google Dorking results successfully saved in TXT file" + Style.RESET_ALL)
+    return "File with gathered links was successfully created"
+
