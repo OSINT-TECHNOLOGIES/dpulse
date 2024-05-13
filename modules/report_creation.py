bd953da93b80317f915fb00df0cc02e3e40abf5e
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-05-13 23:01:36 +0300

    Updated report_creation.py to 0.9rc1

--- modules/report_creation.py
+++ modules/report_creation.py
@@ -22,9 +22,10 @@ try:
     from colorama import Fore, Style
     import webbrowser
     import sqlite3
-except ImportError:
-    print(Fore.RED + "[Error #001 - Import error] Can't import some requirements that are necessary to start DPULSE. Please check that all necessary requirements are installed!" + Style.RESET_ALL)
+except ImportError as e:
+    print(Fore.RED + "Import error appeared. Reason: {}".format(e) + Style.RESET_ALL)
     sys.exit()
+
 def insert_pdf(file):
     with open(file, 'rb') as pdf_file:
         blob_data = pdf_file.read()
@@ -43,8 +44,8 @@ def insert_blob(pdf_blob, db_casename, creation_date, case_comment):
         sqlite_connection.commit()
         print(Fore.GREEN + "Scanning results are successfully saved in report storage database")
         cursor.close()
-    except sqlite3.Error as error:
-        print(Fore.RED + "[Error #002 - Sqlite3 error] Failed to insert scanning results in report storage database", error)
+    except sqlite3.Error as e:
+        print(Fore.RED + "Failed to insert scanning results in report storage database. Reason: {}".format(e))
     finally:
         if sqlite_connection:
             sqlite_connection.close()
@@ -92,8 +93,8 @@ try:
     current_directory = os.path.dirname(current_script)
     cfg_file_path = os.path.join(current_directory, find_files('config.txt'))
     print(Fore.GREEN + 'Main config file was found at {}'.format(cfg_file_path))
-except TypeError:
-    print(Fore.RED + '[Error #003 - No CFG error] Main config file was not found in DPULSE root directory.' + Style.RESET_ALL)
+except TypeError as e:
+    print(Fore.RED + 'Main config file was not found in DPULSE root directory. Reason: {}'.format(e) + Style.RESET_ALL)
     sys.exit()
 
 short_domain = ''
@@ -121,7 +122,7 @@ def create_report(short_domain, url, case_comment):
         db_creation_date = str(now.year) + str(now.month) + str(now.day)
         report_folder = "report_{}".format(foldername)
         os.makedirs(report_folder, exist_ok=True)
-
+        print(Fore.GREEN + "Started scanning domain" + Style.RESET_ALL)
         print(Fore.GREEN + "Getting domain IP address" + Style.RESET_ALL)
         ip = cp.ip_gather(short_domain)
         print(Fore.GREEN + 'Processing WHOIS scanning' + Style.RESET_ALL)
@@ -142,30 +143,34 @@ def create_report(short_domain, url, case_comment):
         sitemap_links_status = np.extract_links_from_sitemap(report_folder)
         print(Fore.GREEN + 'Gathering info about website technologies' + Style.RESET_ALL)
         web_servers, cms, programming_languages, web_frameworks, analytics, javascript_frameworks = np.get_technologies(url)
-        print(Fore.GREEN + 'Processing Shodan InternetDB search')
+        print(Fore.GREEN + 'Processing Shodan InternetDB search' + Style.RESET_ALL)
         ports, hostnames, cpes, tags, vulns = np.query_internetdb(ip)
         print(Fore.GREEN + 'Processing Google Dorking' + Style.RESET_ALL)
         dorking_status = dp.save_results_to_txt(report_folder, dp.get_dorking_query(short_domain))
+
+        common_socials = {key: social_medias.get(key, []) + sd_socials.get(key, []) for key in set(social_medias) | set(sd_socials)}
+
+        for key in common_socials:
+            common_socials[key] = list(set(common_socials[key]))
+
+        total_socials = sum(len(values) for values in common_socials.values())
 
         context = {'sh_domain': short_domain, 'full_url': url, 'ip_address': cp.ip_gather(short_domain),'registrar': res['registrar'],
                                 'creation_date': res['creation_date'],'expiration_date': res['expiration_date'],
                                 'name_servers': ', '.join(res['name_servers']),'org': res['org'],
                                 'mails': cp.mail_gather(url), 'subdomain_mails': subdomain_mails, 'subdomain_socials': social_medias,
-                                'subdomain_ip': subdomain_ip, 'fb_links_s': '  '.join(sd_socials['Facebook']), 'inst_links_s': '  '.join(sd_socials['Instagram']), 'tw_links_s': '  '.join(sd_socials['Twitter']),
-                                'tg_links_s': '  '.join(sd_socials['Telegram']), 'tt_links_s': '  '.join(sd_socials['TikTok']),
-                                'li_links_s': '  '.join(sd_socials['LinkedIn']), 'vk_links_s': '  '.join(sd_socials['VKontakte']), 'yt_links_s': '  '.join(sd_socials['YouTube']),
-                                'wc_links_s': '  '.join(sd_socials['WeChat']), 'ok_links_s': '  '.join(sd_socials['Odnoklassniki']),
-                                'subdomains': ' // '.join(subdomains), 'fb_links': '   '.join(social_medias['Facebook']),
-                                'tw_links': '  '.join(social_medias['Twitter']), 'inst_links': '  '.join(social_medias['Instagram']),
-                                'tg_links': '  '.join(social_medias['Telegram']), 'tt_links': '  '.join(social_medias['TikTok']),
-                                'li_links': '  '.join(social_medias['LinkedIn']), 'vk_links': '  '.join(social_medias['VKontakte']),
-                                'yt_links': '  '.join(social_medias['YouTube']), 'wc_links': '  '.join(social_medias['WeChat']),
-                                'ok_links': '  '.join(social_medias['Odnoklassniki']), 'robots_txt_result': robots_txt_result, 'sitemap_xml_result': sitemap_xml_result, 'dorking_status': dorking_status,
+                                'subdomain_ip': subdomain_ip,
+                                'subdomains': ' // '.join(subdomains), 'fb_links': '   '.join(common_socials['Facebook']),
+                                'tw_links': '  '.join(common_socials['Twitter']), 'inst_links': '  '.join(common_socials['Instagram']),
+                                'tg_links': '  '.join(common_socials['Telegram']), 'tt_links': '  '.join(common_socials['TikTok']),
+                                'li_links': '  '.join(common_socials['LinkedIn']), 'vk_links': '  '.join(common_socials['VKontakte']),
+                                'yt_links': '  '.join(common_socials['YouTube']), 'wc_links': '  '.join(common_socials['WeChat']),
+                                'ok_links': '  '.join(common_socials['Odnoklassniki']), 'robots_txt_result': robots_txt_result, 'sitemap_xml_result': sitemap_xml_result, 'dorking_status': dorking_status,
                                 'sitemap_links': sitemap_links_status, 'web_servers': web_servers, 'cms': cms, 'programming_languages': programming_languages, 'web_frameworks': web_frameworks, 'analytics': analytics,
                                 'javascript_frameworks': javascript_frameworks,
                                  'ctime': ctime, 'a_tsf': subdomains_amount, 'mx_records': mx_records, 'issuer': issuer, 'subject': subject, 'notBefore': notBefore, 'notAfter': notAfter,
                                 'commonName': commonName, 'serialNumber': serialNumber, 'ports': ports, 'hostnames': hostnames, 'cpes': cpes,
-                                'tags': tags, 'vulns': vulns}
+                                'tags': tags, 'vulns': vulns, 'a_tsm': total_socials}
 
         print(Fore.GREEN + 'Processing report for {} case...'.format(short_domain) + Style.RESET_ALL)
         template_loader = jinja2.FileSystemLoader('./')
@@ -178,4 +183,4 @@ def create_report(short_domain, url, case_comment):
         print(Fore.GREEN + "Report for {} case was created at {}".format(''.join(short_domain), ctime) + Style.RESET_ALL)
         insert_blob(insert_pdf(report_file), db_casename, db_creation_date, case_comment)
     except Exception as e:
-        print(Fore.RED + '[Error #005 - Report creation error] Unable to create PDF report. Reason: {}'.format(e))
+        print(Fore.RED + 'Unable to create PDF report. Reason: {}'.format(e))
