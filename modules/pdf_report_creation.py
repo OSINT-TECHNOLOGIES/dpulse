f00579699ccf970a626f98cd735b34ca00ef06f5
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-06-05 02:18:21 +0300

    Updated pdf_report_creation.py to v1.0.1

--- modules/pdf_report_creation.py
+++ modules/pdf_report_creation.py
@@ -1,13 +1,3 @@
-"""
-report_creation module
-
-Contains the function to collect all the outputs from crawl_processor functions and compile them into PDF report
-
-Arguments:
-short_domain: domain name of certain website
-url: http://short_domain/
-"""
-
 import sys
 sys.path.append('service')
 
@@ -50,7 +40,7 @@ except TypeError:
 try:
     current_script = os.path.realpath(__file__)
     current_directory = os.path.dirname(current_script)
-    cfg_file_path = os.path.join(current_directory, fp.find_files('config.txt'))
+    cfg_file_path = os.path.join(current_directory, fp.find_files('dorkslist.txt'))
     print(Fore.GREEN + 'Main config file was found at {}'.format(cfg_file_path))
 except TypeError as e:
     print(Fore.RED + 'Main config file was not found in DPULSE root directory. Reason: {}'.format(e) + Style.RESET_ALL)
@@ -58,9 +48,6 @@ except TypeError as e:
 
 short_domain = ''
 def report_encoding_config():
-    """
-    Function which sets some configurations for PDF report file
-    """
     return {
         'encoding': 'UTF-8',
         'enable-local-file-access': True
@@ -68,10 +55,6 @@ def report_encoding_config():
 
 search_query = []
 def create_report(short_domain, url, case_comment):
-    """
-    Functions which calls all the functions from crawl_processor module and compiles them into PDF report.
-    PDF report will be saved in main script directory
-    """
     try:
         ctime = datetime.now().strftime('%Y-%m-%d_%Hh%Mm%Ss')
         casename = short_domain.replace(".", "") + '_' + ctime + '.pdf'
@@ -84,8 +67,10 @@ def create_report(short_domain, url, case_comment):
         print(Fore.GREEN + "Started scanning domain" + Style.RESET_ALL)
         print(Fore.GREEN + "Getting domain IP address" + Style.RESET_ALL)
         ip = cp.ip_gather(short_domain)
-        print(Fore.GREEN + 'Processing WHOIS scanning' + Style.RESET_ALL)
+        print(Fore.GREEN + 'Gathering WHOIS information' + Style.RESET_ALL)
         res = cp.whois_gather(short_domain)
+        print(Fore.GREEN + 'Processing e-mails gathering' + Style.RESET_ALL)
+        mails = cp.mail_gather(url)
         print(Fore.GREEN + 'Processing subdomain gathering' + Style.RESET_ALL)
         subdomains, subdomains_amount = cp.subdomains_gather(url, short_domain)
         print(Fore.GREEN + 'Processing social medias gathering' + Style.RESET_ALL)
@@ -108,28 +93,27 @@ def create_report(short_domain, url, case_comment):
         dorking_status = dp.save_results_to_txt(report_folder, dp.get_dorking_query(short_domain))
 
         common_socials = {key: social_medias.get(key, []) + sd_socials.get(key, []) for key in set(social_medias) | set(sd_socials)}
-
         for key in common_socials:
             common_socials[key] = list(set(common_socials[key]))
 
         total_socials = sum(len(values) for values in common_socials.values())
 
         context = {'sh_domain': short_domain, 'full_url': url, 'ip_address': cp.ip_gather(short_domain),'registrar': res['registrar'],
-                                'creation_date': res['creation_date'],'expiration_date': res['expiration_date'],
-                                'name_servers': ', '.join(res['name_servers']),'org': res['org'],
-                                'mails': cp.mail_gather(url), 'subdomain_mails': subdomain_mails, 'subdomain_socials': social_medias,
-                                'subdomain_ip': subdomain_ip,
-                                'subdomains': ' // '.join(subdomains), 'fb_links': '   '.join(common_socials['Facebook']),
-                                'tw_links': '  '.join(common_socials['Twitter']), 'inst_links': '  '.join(common_socials['Instagram']),
-                                'tg_links': '  '.join(common_socials['Telegram']), 'tt_links': '  '.join(common_socials['TikTok']),
-                                'li_links': '  '.join(common_socials['LinkedIn']), 'vk_links': '  '.join(common_socials['VKontakte']),
-                                'yt_links': '  '.join(common_socials['YouTube']), 'wc_links': '  '.join(common_socials['WeChat']),
-                                'ok_links': '  '.join(common_socials['Odnoklassniki']), 'robots_txt_result': robots_txt_result, 'sitemap_xml_result': sitemap_xml_result, 'dorking_status': dorking_status,
-                                'sitemap_links': sitemap_links_status, 'web_servers': web_servers, 'cms': cms, 'programming_languages': programming_languages, 'web_frameworks': web_frameworks, 'analytics': analytics,
-                                'javascript_frameworks': javascript_frameworks,
-                                 'ctime': ctime, 'a_tsf': subdomains_amount, 'mx_records': mx_records, 'issuer': issuer, 'subject': subject, 'notBefore': notBefore, 'notAfter': notAfter,
-                                'commonName': commonName, 'serialNumber': serialNumber, 'ports': ports, 'hostnames': hostnames, 'cpes': cpes,
-                                'tags': tags, 'vulns': vulns, 'a_tsm': total_socials}
+                                    'creation_date': res['creation_date'],'expiration_date': res['expiration_date'],
+                                    'name_servers': ', '.join(res['name_servers']),'org': res['org'],
+                                    'mails': mails, 'subdomain_mails': subdomain_mails, 'subdomain_socials': social_medias,
+                                    'subdomain_ip': subdomain_ip,
+                                    'subdomains': ' // '.join(subdomains), 'fb_links': '   '.join(common_socials['Facebook']),
+                                    'tw_links': '  '.join(common_socials['Twitter']), 'inst_links': '  '.join(common_socials['Instagram']),
+                                    'tg_links': '  '.join(common_socials['Telegram']), 'tt_links': '  '.join(common_socials['TikTok']),
+                                    'li_links': '  '.join(common_socials['LinkedIn']), 'vk_links': '  '.join(common_socials['VKontakte']),
+                                    'yt_links': '  '.join(common_socials['YouTube']), 'wc_links': '  '.join(common_socials['WeChat']),
+                                    'ok_links': '  '.join(common_socials['Odnoklassniki']), 'robots_txt_result': robots_txt_result, 'sitemap_xml_result': sitemap_xml_result, 'dorking_status': dorking_status,
+                                    'sitemap_links': sitemap_links_status, 'web_servers': web_servers, 'cms': cms, 'programming_languages': programming_languages, 'web_frameworks': web_frameworks, 'analytics': analytics,
+                                    'javascript_frameworks': javascript_frameworks,
+                                     'ctime': ctime, 'a_tsf': subdomains_amount, 'mx_records': mx_records, 'issuer': issuer, 'subject': subject, 'notBefore': notBefore, 'notAfter': notAfter,
+                                    'commonName': commonName, 'serialNumber': serialNumber, 'ports': ports, 'hostnames': hostnames, 'cpes': cpes,
+                                    'tags': tags, 'vulns': vulns, 'a_tsm': total_socials}
 
         print(Fore.GREEN + 'Processing report for {} case...'.format(short_domain) + Style.RESET_ALL)
         template_loader = jinja2.FileSystemLoader('./')
