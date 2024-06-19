ee4b67040160e28e6f0b206474f556f40bf88423
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-06-19 22:48:26 +0300

    Backuped installer.bat [developer error]

--- installer.bat
+++ installer.bat
@@ -1,20 +1,66 @@
-#!/bin/bash
+@echo off
+set /p UserInput=Do you want to start installer? [Y/N]: 
+if /I "%UserInput%" neq "Y" goto End
 
-echo "Select appropriate menu item"
-echo "[1] Clone repository and install requirements (requires installed Git)"
-echo "[2] Only install requirements"
-read -p "Enter the number: " UserChoice
+echo.
 
-if [ "$UserChoice" == "1" ]; then
-    echo "Cloning repository and installing requirements..."
-    git clone https://github.com/OSINT-TECHNOLOGIES/dpulse
-    cd dpulse
-    pip install -r requirements.txt
-elif [ "$UserChoice" == "2" ]; then
-    echo "Installing requirements..."
-    pip install -r requirements.txt
-else
-    echo "Incorrect choice."
-fi
+:CheckGit
+where git >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Git is not installed. Please install Git first.
+    set GIT_NOT_INSTALLED=1
+) else (
+    set GIT_NOT_INSTALLED=0
+)
 
-echo "Installation end."
+echo.
+
+:Menu
+echo Select appropriate menu item
+echo.
+echo [1] Clone repository and install requirements (requires installed Git)
+echo [2] Only install requirements
+echo.
+set /p UserChoice=Enter the number: 
+
+echo.
+
+if "%UserChoice%"=="1" (
+    if %GIT_NOT_INSTALLED%==1 (
+        echo You cannot clone the repository because Git is not installed.
+        echo.
+        goto Menu
+    )
+    call :CloneAndInstall
+) else if "%UserChoice%"=="2" (
+    call :InstallDependencies
+) else (
+    echo Incorrect choice.
+    echo.
+    goto Menu
+)
+
+goto End
+
+:CloneAndInstall
+echo Cloning repository and installing requirements...
+git clone https://github.com/OSINT-TECHNOLOGIES/dpulse
+cd dpulse
+pip install -r requirements.txt
+
+echo.
+
+goto End
+
+:InstallDependencies
+echo Installing requirements...
+pip install -r requirements.txt
+
+echo.
+
+goto End
+
+:End
+echo Installation end.
+echo.
+pause
