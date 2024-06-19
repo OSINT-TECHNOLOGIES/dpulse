20b7a524ea6763f6aa90c89b690904ace688a4e6
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-06-19 22:48:47 +0300

    Updated installer.sh with new variant

--- installer.sh
+++ installer.sh
@@ -1,51 +1,20 @@
 #!/bin/bash
 
-read -p "Do you want to start the installer? [Y/N]: " UserInput
-if [[ "${UserInput^^}" != "Y" ]]; then
-    exit 0
-fi
+echo "Select appropriate menu item"
+echo "[1] Clone repository and install requirements (requires installed Git)"
+echo "[2] Only install requirements"
+read -p "Enter the number: " UserChoice
 
-echo
-
-Menu() {
-    echo "Select an appropriate menu item:"
-    echo "[1] Clone repository and install requirements"
-    echo "[2] Only install requirements"
-    read -p "Enter the number: " UserChoice
-    echo
-
-    if [[ "${UserChoice}" == "1" ]]; then
-        CloneAndInstall
-    elif [[ "${UserChoice}" == "2" ]]; then
-        InstallDependencies
-    else
-        echo "Incorrect choice."
-        echo
-        Menu
-    fi
-}
-
-CloneAndInstall() {
+if [ "$UserChoice" == "1" ]; then
     echo "Cloning repository and installing requirements..."
     git clone https://github.com/OSINT-TECHNOLOGIES/dpulse
-    cd dpulse || exit 1
+    cd dpulse
     pip install -r requirements.txt
-
-    echo
-}
-
-InstallDependencies() {
+elif [ "$UserChoice" == "2" ]; then
     echo "Installing requirements..."
     pip install -r requirements.txt
+else
+    echo "Incorrect choice."
+fi
 
-    echo
-}
-
-End() {
-    echo "Installation end."
-    echo
-    read -p "Press Enter to continue..."
-}
-
-Menu
-End
+echo "Installation end."
