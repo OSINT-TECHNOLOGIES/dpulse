51f4070a75cdce339db446e0493e76bb7b2c2513
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-07-18 19:30:47 +0300

    Added new argument to support keywords search decline (rel. to #53)

--- pagesearch/pagesearch_main.py
+++ pagesearch/pagesearch_main.py
@@ -1,7 +1,7 @@
 from colorama import Fore, Style
 from pagesearch_parsers import subdomains_parser
 
-def normal_search(to_search_array, report_folder, keywords):
+def normal_search(to_search_array, report_folder, keywords, keywords_flag):
     print(Fore.GREEN + "Conducting PageSearch. Please, be patient, it may take a long time\n" + Style.RESET_ALL)
-    subdomains_parser(to_search_array[0], report_folder, keywords)
+    subdomains_parser(to_search_array[0], report_folder, keywords, keywords_flag)
 
