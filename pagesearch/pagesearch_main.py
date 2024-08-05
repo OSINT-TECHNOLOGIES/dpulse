126aa1036958cbf70d0ab33f13fa00d33c913def
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-08-05 21:23:07 +0300

    Added e-mails return from PageSearch (#59)

--- pagesearch/pagesearch_main.py
+++ pagesearch/pagesearch_main.py
@@ -3,5 +3,6 @@ from pagesearch_parsers import subdomains_parser
 
 def normal_search(to_search_array, report_folder, keywords, keywords_flag):
     print(Fore.GREEN + "Conducting PageSearch. Please, be patient, it may take a long time\n" + Style.RESET_ALL)
-    subdomains_parser(to_search_array[0], report_folder, keywords, keywords_flag)
+    ps_emails_return = subdomains_parser(to_search_array[0], report_folder, keywords, keywords_flag)
+    return ps_emails_return
 
