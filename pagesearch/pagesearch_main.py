590c7a34e51fbe3efdfc60726690ba020de140ed
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-08-09 22:46:55 +0300

    Added new function to inspect sitemap links

--- pagesearch/pagesearch_main.py
+++ pagesearch/pagesearch_main.py
@@ -1,8 +1,14 @@
 from colorama import Fore, Style
 from pagesearch_parsers import subdomains_parser
+from pagesearch_deepsearch import sitemap_inspection
 
 def normal_search(to_search_array, report_folder, keywords, keywords_flag):
     print(Fore.GREEN + "Conducting PageSearch. Please, be patient, it may take a long time\n" + Style.RESET_ALL)
     ps_emails_return = subdomains_parser(to_search_array[0], report_folder, keywords, keywords_flag)
     return ps_emails_return
+
+def sitemap_inspection_search(report_folder):
+    print(Fore.GREEN + "Conducting PageSearch in Sitemap Inspection mode. Please, be patient, it will take a long time\n" + Style.RESET_ALL)
+    ds_emails_return = sitemap_inspection(report_folder)
+    return ds_emails_return
 
