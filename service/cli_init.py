d93aa84c84bc9b2f6c33f928ea621b272e0e0f95
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-08-12 18:48:36 +0300

    Updated "Help" menu content

--- service/cli_init.py
+++ service/cli_init.py
@@ -37,10 +37,13 @@ class Menu:
 
     def print_help_menu(self):
         print(Fore.MAGENTA + Back.WHITE + '[HELP MENU]' + Style.RESET_ALL)
-        print(Fore.BLACK + Back.WHITE + 'Be advised that choosing any of points below will open your web browser!' + Style.RESET_ALL)
-        print(Fore.CYAN + "1. How to correctly input your targets URL in DPULSE")
-        print(Fore.CYAN + "2. DPULSE CLI colors and their meanings")
-        print(Fore.LIGHTRED_EX + "3. Return to main menu" + Style.RESET_ALL + '\n')
+        print(Fore.BLACK + Back.WHITE + '\nBe advised that choosing any of points below will open your web browser!\n' + Style.RESET_ALL)
+        print(Fore.CYAN + "1. Open DPULSE repository")
+        print(Fore.CYAN + "2. Open DPULSE wiki")
+        print(Fore.CYAN + "3. How to correctly input your targets in DPULSE")
+        print(Fore.CYAN + "4. PageSearch user guide")
+        print(Fore.CYAN + "5. Report storage database user guide")
+        print(Fore.LIGHTRED_EX + "6. Return to main menu" + Style.RESET_ALL + '\n')
 
     def print_db_menu(self):
         print('\n')
