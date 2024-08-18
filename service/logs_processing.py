38daf8182f064aa5cf281b3351fba1d72b3dc5fa
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-08-19 00:02:15 +0300

    Improved and cleaned code of logs_processing.py (part of #58)

--- service/logs_processing.py
+++ service/logs_processing.py
@@ -6,23 +6,20 @@ def create_log_folder():
     os.makedirs(scan_logs_folder, exist_ok=True)
     return scan_logs_folder
 
-def write_logs(ctime, whois_gather_status, contact_mail_gather_status, subdomains_gather_status, list_to_log):
-    to_log = [whois_gather_status, contact_mail_gather_status, subdomains_gather_status]
+def write_logs(ctime, whois_gather_status, contact_mail_gather_status, subdomains_gather_status, list_to_log, get_ssl_certificate_status, get_dns_info_status, get_sitemap_xml_status, extract_links_from_sitemap_status):
+    to_log = [whois_gather_status, contact_mail_gather_status, subdomains_gather_status, get_ssl_certificate_status, get_dns_info_status, get_sitemap_xml_status, extract_links_from_sitemap_status]
     subdomains_processes_logs = [item for sublist in list_to_log for item in sublist]
     scan_logs_folder = create_log_folder()
-    logging.basicConfig(level=logging.INFO, filename=scan_logs_folder + f"//scan_log_{ctime}.log", filemode="w", format="%(asctime)s %(message)s")
+    log_file_name = f"scan_log_{ctime}.log"
+    logging.basicConfig(level=logging.INFO, filename=scan_logs_folder + "// " + log_file_name, filemode="w", format="%(asctime)s %(message)s")
     logging.info("# THIS FILE REPRESENTS DPULSE SCAN LOGS\n")
     print(Fore.GREEN + "Created log file for this scan" + Style.RESET_ALL)
 
     for log in to_log:
-        if log[-2:] == "OK":
-            logging.info(log)
-        else:
-            logging.info(log)
+        logging.info(log)
 
     for log in subdomains_processes_logs:
-        if log[-2:] == "OK":
-            logging.info(log)
-        else:
-            logging.info(log)
+        logging.info(log)
+
     logging.shutdown()
+    return log_file_name
