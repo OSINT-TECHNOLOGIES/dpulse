f5f27fcc8088c5427ef749ebb35196ef45355a09
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-08-22 02:26:55 +0300

    Reworked logs into solid journal file for all scans

--- pagesearch/pagesearch_main.py
+++ pagesearch/pagesearch_main.py
@@ -4,9 +4,8 @@ from pagesearch_deepsearch import sitemap_inspection
 
 def normal_search(to_search_array, report_folder, keywords, keywords_flag):
     print(Fore.GREEN + "Conducting PageSearch. Please, be patient, it may take a long time\n" + Style.RESET_ALL)
-    ps_emails_return, extract_text_from_pdf_status, find_keywords_in_pdfs_status = subdomains_parser(to_search_array[0], report_folder, keywords, keywords_flag)
-    ps_to_log_list = [extract_text_from_pdf_status, find_keywords_in_pdfs_status]
-    return ps_emails_return, ps_to_log_list
+    ps_emails_return = subdomains_parser(to_search_array[0], report_folder, keywords, keywords_flag)
+    return ps_emails_return
 
 def sitemap_inspection_search(report_folder):
     print(Fore.GREEN + "Conducting PageSearch in Sitemap Inspection mode. Please, be patient, it will take a long time\n" + Style.RESET_ALL)
