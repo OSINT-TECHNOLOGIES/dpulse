8c4c26c1f807517baa0595ab6c01625f788c0fcc
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-08-24 17:24:01 +0300

    Normalized output of emails, extended range of bad symbols to clean

--- reporting_modules/xlsx_report_creation.py
+++ reporting_modules/xlsx_report_creation.py
@@ -59,10 +59,14 @@ def create_report(short_domain, url, case_comment, data_array, report_info_array
         if len(ps_emails_return) > 0:
             subdomain_mails += ps_emails_return
             subdomain_mails = list(set(subdomain_mails))
+            subdomain_mails_cleaned = []
             substrings = ['m=Base64', 'Ë','Á','Æ','Å','Ä','Ò','Á','ó','ð','É','ë','â']
             for substring in substrings:
                 if any(substring in s for s in subdomain_mails):
                     subdomain_mails.remove(next(s for s in subdomain_mails if substring in s))
+            for email in subdomain_mails:
+                new_emails = email.split(', ')
+                subdomain_mails_cleaned.extend(new_emails)
 
         wb = openpyxl.Workbook()
         sheet_names = [
