1badbcf095e5449d7f5be831419ce84a8d84f910
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-09-23 00:37:55 +0300

    Stabilized Google Dorking module, optimized outputs, returns and file writing

--- dorking/dorking_handler.py
+++ dorking/dorking_handler.py
@@ -1,38 +1,77 @@
-import mechanicalsoup
-from bs4 import BeautifulSoup
-import requests
-from colorama import Fore, Style
+import sys
 
-def scrape_dork(dork, short_domain):
-    browser = mechanicalsoup.Browser(soup_config={'features': 'html.parser'})
-    modified_dork = dork.replace("{}", f"{short_domain}")
-    print(Fore.GREEN + f"Dorking with " + Style.RESET_ALL + Fore.LIGHTCYAN_EX + f"{modified_dork}" + Style.RESET_ALL + Fore.GREEN + " dork" + Style.RESET_ALL)
-    search_result = browser.get(f"https://www.google.com/search?q={modified_dork}")
-    soup = BeautifulSoup(search_result.content, "html.parser")
-    results = soup.find_all("a", href=True)
-    full_urls = []
-    for result in results:
-        relative_link = result['href']
-        if relative_link.startswith("/"):
-            pass
-        else:  # Sometimes Google might give an already full URL
-            full_urls.append(relative_link)
+try:
+    import requests.exceptions
+    from colorama import Fore, Style
+    import mechanicalsoup
+    import re
+    import requests
+    import sqlite3
+except ImportError as e:
+    print(Fore.RED + "Import error appeared. Reason: {}".format(e) + Style.RESET_ALL)
+    sys.exit()
 
-    return full_urls
-
-def composing_dorking(dorks, conn, short_domain, report_path):
-    results_file = open(f"{report_path}//dork_results.txt", "w")
-    for dork_id, dork in dorks:
-        results = scrape_dork(dork, short_domain)
-        #print(Fore.GREEN + f"\nDORK #{dork_id}: {dork}")
-        results_file.write(f"\nDORK #{dork_id}: {dork}\n")
-        if results:
-            for result in results:
-                #print(f"=> {result_link}")
-                results_file.write(f"=> {result}\n")
-            print(Fore.LIGHTGREEN_EX + "-------------------------------------------------" + Style.RESET_ALL)
-        else:
-            #print("No results found for this dork.")
-            results_file.write("No results found for this dork.\n")
+def get_dorking_query(short_domain, dorking_db_path, table):
+    print(Fore.GREEN + "Getting dorking query from database")
+    conn = sqlite3.connect(dorking_db_path)
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT dork FROM {table}")
+    rows = cursor.fetchall()
+    search_query = [row[0].format(short_domain) for row in rows]
     conn.close()
-    results_file.close()
+    return search_query
+
+def solid_google_dorking(query, pages=100):
+    try:
+        browser = mechanicalsoup.StatefulBrowser()
+        browser.open("https://www.google.com/")
+        browser.select_form('form[action="/search"]')
+        browser["q"] = str(query)
+        browser.submit_selected(btnName="btnG")
+        result_query = []
+        for page in range(pages):
+            for link in browser.links():
+                target = link.attrs['href']
+                if (target.startswith('/url?') and not
+                target.startswith("/url?q=http://webcache.googleusercontent.com")):
+                    target = re.sub(r"^/url\?q=([^&]*)&.*", r"\1", target)
+                    result_query.append(target)
+            try:
+                browser.follow_link(nr=page + 1)
+            except mechanicalsoup.LinkNotFoundError:
+                break
+        del result_query[-2:]
+        return result_query
+    except requests.exceptions.ConnectionError as e:
+        print(Fore.RED + "Error while establishing connection with domain. No results will appear. Reason: {}".format(e) + Style.RESET_ALL)
+        #return "Google Dorking results file was not created"
+
+def save_results_to_txt(folderpath, queries, pages=10):
+    txt_writepath = folderpath + '//04-dorking_results.txt'
+    with open(txt_writepath, 'w') as f:
+        for i, query in enumerate(queries, start=1):
+            f.write(f"QUERY #{i}: {query}\n")
+            results = solid_google_dorking(query, pages)
+            print(Fore.GREEN + f"Dorking with {query} dork")
+            if not results:
+                f.write("=> NO RESULT FOUND\n")
+                print(Fore.RED + f"No results were found for {query} dork")
+            else:
+                for result in results:
+                    f.write(f"=> {result}\n")
+            f.write("\n")
+    print(Fore.GREEN + "Google Dorking results successfully saved in TXT file" + Style.RESET_ALL)
+    #return "File with gathered links was successfully created"
+
+def transfer_results_to_xlsx(queries, pages=10):
+    dorking_return_list = []
+    for i, query in enumerate(queries, start=1):
+        dorking_return_list.append(f"QUERY #{i}: {query}\n")
+        results = solid_google_dorking(query, pages)
+        if not results:
+            dorking_return_list.append("NO RESULT FOUND\n")
+        else:
+            for result in results:
+                dorking_return_list.append(f"{result}\n")
+        dorking_return_list.append("\n")
+    #return "File with gathered links was successfully created", dorking_return_list
