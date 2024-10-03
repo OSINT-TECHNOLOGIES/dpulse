07eeed57cef09ff4acc0ece6647f8120d507d526
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-10-04 01:30:14 +0300

    Updated CLI visual

--- service/cli_init.py
+++ service/cli_init.py
@@ -38,8 +38,8 @@ class Menu:
         print(Fore.MAGENTA + Back.WHITE + '[SETTINGS MENU]' + Style.RESET_ALL)
         print(Fore.CYAN + "1. Print current config file")
         print(Fore.CYAN + "2. Edit config file")
-        print(Fore.CYAN + "3. Generate custom Dorking DB")
-        print(Fore.CYAN + "4. Add API key for existing API")
+        print(Fore.CYAN + "3. Generate custom Dorking DB (not active)")
+        print(Fore.CYAN + "4. Add API key for existing API (not active)")
         print(Fore.LIGHTRED_EX + "5. Return to main menu" + Style.RESET_ALL + '\n')
 
     def print_help_menu(self):
