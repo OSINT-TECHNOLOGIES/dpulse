d40eea1425272873f95ac05db0c82e590e17f1fc
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-10-04 00:39:15 +0300

    Added config parameters for Google Dorking module

--- service/config_processing.py
+++ service/config_processing.py
@@ -5,6 +5,7 @@ def create_config():
     config = configparser.ConfigParser()
     config['LOGGING'] = {'log_level': 'info'}
     config['CLI VISUAL'] = {'preview_color': 'red', 'font': 'slant'}
+    config['DORKING'] = {'dorking_delay (secs)': '2', 'delay_step': '5'}
 
     with open('service//config.ini', 'w') as configfile:
         config.write(configfile)
@@ -19,11 +20,15 @@ def read_config():
     log_level = config.get('LOGGING', 'log_level')
     cli_preview_color = config.get('CLI VISUAL', 'preview_color')
     wm_font = config.get('CLI VISUAL', 'font')
+    dorking_delay = config.get('DORKING', 'dorking_delay (secs)')
+    delay_step = config.get('DORKING', 'delay_step')
 
     config_values = {
         'logging_level': log_level,
         'preview_color': cli_preview_color,
-        'wm_font': wm_font
+        'wm_font': wm_font,
+        'dorking_delay (secs)': dorking_delay,
+        'delay_step': delay_step
     }
 
     return config_values
