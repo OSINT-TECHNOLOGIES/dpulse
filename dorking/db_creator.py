c5b4ffb6019a0a3ed952708cdf064fe13257a8cb
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-10-14 18:27:00 -0400

    Added separate module to handle custom dorking db generation

---
+++ dorking/db_creator.py
@@ -0,0 +1,31 @@
+import sqlite3
+from colorama import Fore
+
+def manage_dorks(db_name):
+    db_prep_string = str(db_name) + '.db'
+    conn = sqlite3.connect('dorking//' + str(db_prep_string))
+    cursor = conn.cursor()
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS dorks (
+        dork_id INTEGER PRIMARY KEY,
+        dork TEXT NOT NULL
+    )
+    ''')
+    conn.commit()
+
+    def add_dork(dork_id, dork):
+        try:
+            cursor.execute('INSERT INTO dorks (dork_id, dork) VALUES (?, ?)', (dork_id, dork))
+            conn.commit()
+            print(Fore.GREEN + "Successfully added new dork")
+        except sqlite3.IntegrityError:
+            print(Fore.RED + "Attention, dork_id variable must be unique")
+
+    while True:
+        dork_id = input(Fore.YELLOW + "Enter dork_id (or 'q' to quit this mode and save changes) >> ")
+        if dork_id.lower() == 'q':
+            break
+        dork = input(Fore.YELLOW + "Enter new dork >> ")
+        add_dork(int(dork_id), dork)
+    conn.close()
