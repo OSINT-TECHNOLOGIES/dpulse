711fa9dcdef4f080e1e13940d55141008b49ff5b
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-10-15 19:43:06 +0300

    Added check on existent custom DB name

--- dorking/db_creator.py
+++ dorking/db_creator.py
@@ -1,31 +1,36 @@
 import sqlite3
 from colorama import Fore
+import os
 
 def manage_dorks(db_name):
     db_prep_string = str(db_name) + '.db'
-    conn = sqlite3.connect('dorking//' + str(db_prep_string))
-    cursor = conn.cursor()
+    if os.path.exists('dorking//' + db_prep_string):
+        print(Fore.RED + f"Sorry, but {db_prep_string} database is already exists. Choose other name for your custom DB")
+        pass
+    else:
+        conn = sqlite3.connect('dorking//' + str(db_prep_string))
+        cursor = conn.cursor()
 
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS dorks (
-        dork_id INTEGER PRIMARY KEY,
-        dork TEXT NOT NULL
-    )
-    ''')
-    conn.commit()
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS dorks (
+            dork_id INTEGER PRIMARY KEY,
+            dork TEXT NOT NULL
+        )
+        ''')
+        conn.commit()
 
-    def add_dork(dork_id, dork):
-        try:
-            cursor.execute('INSERT INTO dorks (dork_id, dork) VALUES (?, ?)', (dork_id, dork))
-            conn.commit()
-            print(Fore.GREEN + "Successfully added new dork")
-        except sqlite3.IntegrityError:
-            print(Fore.RED + "Attention, dork_id variable must be unique")
-
-    while True:
-        dork_id = input(Fore.YELLOW + "Enter dork_id (or 'q' to quit this mode and save changes) >> ")
-        if dork_id.lower() == 'q':
-            break
-        dork = input(Fore.YELLOW + "Enter new dork >> ")
-        add_dork(int(dork_id), dork)
-    conn.close()
+        def add_dork(dork_id, dork):
+            try:
+                cursor.execute('INSERT INTO dorks (dork_id, dork) VALUES (?, ?)', (dork_id, dork))
+                conn.commit()
+                print(Fore.GREEN + "Successfully added new dork")
+            except sqlite3.IntegrityError:
+                print(Fore.RED + "Attention, dork_id variable must be unique")
+    
+        while True:
+            dork_id = input(Fore.YELLOW + "Enter dork_id (or 'q' to quit this mode and save changes) >> ")
+            if dork_id.lower() == 'q':
+                break
+            dork = input(Fore.YELLOW + "Enter new dork >> ")
+            add_dork(int(dork_id), dork)
+        conn.close()
