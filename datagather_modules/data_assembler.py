ba2c61f6ad8a0d335c9eb9cb68026287f317030d
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-10-16 00:49:16 +0300

    Code refactored / admins_dorks table support

--- datagather_modules/data_assembler.py
+++ datagather_modules/data_assembler.py
@@ -22,26 +22,30 @@ except ImportError as e:
     sys.exit()
 
 def establishing_dork_db_connection(dorking_flag):
-    if dorking_flag == 'basic':
-        dorking_db_path = 'dorking//basic_dorking.db'
-        table = 'basic_dorks'
-    elif dorking_flag == 'iot':
-        dorking_db_path = 'dorking//iot_dorking.db'
-        table = 'iot_dorks'
-    elif dorking_flag == 'files':
-        dorking_db_path = 'dorking//files_dorking.db'
-        table = 'files_dorks'
-    elif dorking_flag == 'admins':
-        dorking_db_path = 'dorking//adminpanels_dorking.db'
-        table = 'adminpanels_dorks'
-    elif dorking_flag == 'web':
-        dorking_db_path = 'dorking//webstructure_dorking.db'
-        table = 'webstructure_dorks'
+    dorking_db_paths = {
+        'basic': 'dorking//basic_dorking.db',
+        'iot': 'dorking//iot_dorking.db',
+        'files': 'dorking//files_dorking.db',
+        'admins': 'dorking//adminpanels_dorking.db',
+        'web': 'dorking//webstructure_dorking.db',
+    }
+    dorking_tables = {
+        'basic': 'basic_dorks',
+        'iot': 'iot_dorks',
+        'files': 'files_dorks',
+        'admins': 'admins_dorks',
+        'web': 'webstructure_dorks',
+    }
+    if dorking_flag in dorking_db_paths:
+        dorking_db_path = dorking_db_paths[dorking_flag]
+        table = dorking_tables[dorking_flag]
     elif dorking_flag.startswith('custom'):
         lst = dorking_flag.split('+')
         dorking_db_name = lst[1]
         dorking_db_path = 'dorking//' + dorking_db_name
         table = 'dorks'
+    else:
+        raise ValueError(f"Invalid dorking flag: {dorking_flag}")
     return dorking_db_path, table
 
 class DataProcessing():
@@ -49,20 +53,15 @@ class DataProcessing():
         report_ctime = datetime.now().strftime('%d-%m-%Y, %H:%M:%S')
         files_ctime = datetime.now().strftime('(%d-%m-%Y, %Hh%Mm%Ss)')
         files_body = short_domain.replace(".", "") + '_' + files_ctime
-        if report_file_type == 'pdf':
-            casename = files_body + '.pdf'
-        elif report_file_type == 'xlsx':
-            casename = files_body + '.xlsx'
-        elif report_file_type == 'html':
-            casename = files_body + '.html'
+        casename = f"{files_body}.{report_file_type}"
         foldername = files_body
         db_casename = short_domain.replace(".", "")
         now = datetime.now()
         db_creation_date = str(now.year) + str(now.month) + str(now.day)
-        report_folder = "report_{}".format(foldername)
-        robots_filepath = report_folder + '//01-robots.txt'
-        sitemap_filepath = report_folder + '//02-sitemap.txt'
-        sitemap_links_filepath = report_folder + '//03-sitemap_links.txt'
+        report_folder = f"report_{foldername}"
+        robots_filepath = os.path.join(report_folder, '01-robots.txt')
+        sitemap_filepath = os.path.join(report_folder, '02-sitemap.txt')
+        sitemap_links_filepath = os.path.join(report_folder, '03-sitemap_links.txt')
         os.makedirs(report_folder, exist_ok=True)
         return casename, db_casename, db_creation_date, robots_filepath, sitemap_filepath, sitemap_links_filepath, report_file_type, report_folder, files_ctime, report_ctime
 
