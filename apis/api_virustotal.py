14131e1c7ca26bf6645cb4a3aab021b6d0f083df
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-10-17 12:43:31 -0400

    Added basic VirusTotal API support

---
+++ apis/api_virustotal.py
@@ -0,0 +1,43 @@
+import requests
+import sqlite3
+from colorama import Fore, Style
+
+def check_domain(domain, api_key):
+    url = "https://www.virustotal.com/vtapi/v2/domain/report"
+    params = {
+        'domain': domain,
+        'apikey': api_key
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+
+def api_virustotal_check(domain):
+    conn = sqlite3.connect('apis//api_keys.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT api_name, api_key FROM api_keys")
+    rows = cursor.fetchall()
+    for row in rows:
+        api_name, api_key = row
+        if api_name == 'VirusTotal':
+            api_key = str(row[1])
+            print(Fore.GREEN + 'Found VirusTotal API key')
+
+    result = check_domain(domain, api_key)
+
+    if result:
+        print(Fore.GREEN + "VIRUSTOTAL DOMAIN REPORT")
+        print(Fore.GREEN + f"Domain: {result.get('domain')}")
+        print(Fore.GREEN + f"Categories: {result.get('categories')}")
+        print(Fore.GREEN + f"Detected URLs: {len(result.get('detected_urls', []))}")
+        print(Fore.GREEN + f"Detected Samples: {len(result.get('detected_samples', []))}")
+        print(Fore.GREEN + f"Undetected Samples: {len(result.get('undetected_samples', []))}")
+    else:
+        print(Fore.RED + "Failed to get domain report")
+
