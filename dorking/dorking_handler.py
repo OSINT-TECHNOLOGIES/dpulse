31fe4a293f5fd22d783a7aaaba97f86300f4aaaa
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-11-20 01:27:26 +0300

    Moved Dorking DB related functions to db_creator.py

--- dorking/dorking_handler.py
+++ dorking/dorking_handler.py
@@ -16,34 +16,6 @@ except ImportError as e:
     print(Fore.RED + "Import error appeared. Reason: {}".format(e) + Style.RESET_ALL)
     sys.exit()
 
-def get_dorking_query(short_domain, dorking_db_path, table):
-    print(Fore.GREEN + "Getting dorking query from database")
-    try:
-        conn = sqlite3.connect(dorking_db_path)
-        cursor = conn.cursor()
-        cursor.execute(f"SELECT dork FROM {table}")
-        rows = cursor.fetchall()
-        search_query = [row[0].format(short_domain) for row in rows]
-        conn.close()
-        return search_query
-    except Exception as e:
-        print(Fore.RED + f"Error getting dorking query: {e}")
-        return []
-        pass
-
-def get_columns_amount(dorking_db_path, table):
-    try:
-        conn = sqlite3.connect(dorking_db_path)
-        cursor = conn.cursor()
-        cursor.execute(f"SELECT COUNT(*) FROM {table}")
-        row_count = cursor.fetchone()[0]
-    except Exception as e:
-        print(f"Error getting column count: {e}")
-        return None
-    finally:
-        conn.close()
-    return row_count
-
 def solid_google_dorking(query, dorking_delay, delay_step, pages=100):
     try:
         browser = mechanicalsoup.StatefulBrowser()
