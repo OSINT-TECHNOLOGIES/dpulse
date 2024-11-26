e3fc3a2770b2a4224b5e9cb9ec7c8b99455a7d22
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-11-26 12:57:00 +0300

    Added CLI output for user-agent changing

--- dorking/ua_rotator.py
+++ dorking/ua_rotator.py
@@ -2,6 +2,7 @@ import random
 import sys
 sys.path.append('service')
 from config_processing import read_config
+from colorama import Fore, Style
 
 class UserAgentRotator:
     def __init__(self):
@@ -9,6 +10,7 @@ class UserAgentRotator:
         self.user_agents = config_values['user_agents']
 
     def get_random_user_agent(self):
+        print(Fore.GREEN + "Changed User-Agent to " + Style.RESET_ALL + Fore.LIGHTCYAN_EX + Style.BRIGHT + f"{random.choice(self.user_agents)}" + Style.RESET_ALL)
         return random.choice(self.user_agents)
 
 user_agent_rotator = UserAgentRotator()
