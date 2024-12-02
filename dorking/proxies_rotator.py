7a25cc5520d08fe5a729d5ad78be27a094cadc65
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-12-03 01:57:51 +0300

    Added function to check on working proxies

--- dorking/proxies_rotator.py
+++ dorking/proxies_rotator.py
@@ -3,13 +3,33 @@ import sys
 sys.path.append('service')
 from config_processing import read_config
 from colorama import Fore, Style
+import requests
+from requests.exceptions import ProxyError, ConnectionError, Timeout
 
 class ProxiesRotator:
     def __init__(self):
         config_values = read_config()
-        self.proxy_file_path = config_values['proxies_file_path']
+        self.proxy_file_path = str(config_values['proxies_file_path'])
 
-    def check_proxy(self):
+    def check_proxies(self, proxies_list):
+        working_proxies = []
+        for proxy in proxies_list:
+            proxies = {
+                "http": proxy,
+                "https": proxy
+            }
+            try:
+                response = requests.get('https://google.com', proxies=proxies, timeout=5)
+                if response.status_code == 200:
+                    working_proxies.append(proxy)
+                    print(Fore.GREEN + f"Proxy {proxy} is working" + Style.RESET_ALL)
+                else:
+                    print(Fore.GREEN +f"Proxy {proxy} returned status code {response.status_code}" + Style.RESET_ALL)
+            except (ProxyError, ConnectionError, Timeout):
+                print(Fore.GREEN + f"Proxy {proxy} is not working" + Style.RESET_ALL)
+        return working_proxies
+
+    def get_proxies(self):
         if self.proxy_file_path == 'NONE':
             print(Fore.RED + "Path to file with proxies was not set in config file. Proxification of Google Dorking won't be applied" + Style.RESET_ALL)
             return 0, ""
