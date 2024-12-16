079a8c916098484bc24ef9bfbbbfdd23ff799532
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2024-12-16 05:51:25 +0300

    CLI color scheme improvements

--- service/cli_init.py
+++ service/cli_init.py
@@ -91,11 +91,35 @@ def print_api_db_msg():
     print(Fore.GREEN + "Examples: related:{}, site:{} inurl:login and so on\n" + Style.RESET_ALL)
 
 def print_ps_cli_report(subdomains_list,  accessible_subdomains, ps_emails_return, files_counter, cookies_counter, api_keys_counter, website_elements_counter, exposed_passwords_counter):
-    print(Fore.GREEN + f"\nDuring subdomains analysis:\n[+] Total {len(subdomains_list)} subdomains were checked")
-    print(Fore.GREEN + f"[+] Among them, {accessible_subdomains} subdomains were accessible")
-    print(Fore.GREEN + f"[+] In result, {len(ps_emails_return)} unique e-mail addresses were found")
-    print(Fore.GREEN + f"[+] Also, {files_counter} files were extracted")
-    print(Fore.GREEN + f"[+] Found {cookies_counter} cookies with values")
-    print(Fore.GREEN + f"[+] Found {api_keys_counter} API keys")
-    print(Fore.GREEN + f"[+] Found {website_elements_counter} different web page elements")
-    print(Fore.GREEN + f"[+] Found {exposed_passwords_counter} exposed passwords")
+    if len(subdomains_list) == 0:
+        print(Fore.GREEN + "\nDuring subdomains analysis:\n[+] Total " + Fore.LIGHTRED_EX + Style.BRIGHT + f"{len(subdomains_list)}" + Style.RESET_ALL + Fore.GREEN + " subdomains were checked" + Style.RESET_ALL)
+    else:
+        print(Fore.GREEN + "\nDuring subdomains analysis:\n[+] Total " + Fore.LIGHTCYAN_EX + Style.BRIGHT + f"{len(subdomains_list)}" + Style.RESET_ALL + Fore.GREEN + " subdomains were checked" + Style.RESET_ALL)
+    if accessible_subdomains == 0:
+        print(Fore.GREEN + "[+] Among them " + Fore.LIGHTRED_EX + Style.BRIGHT + f"{accessible_subdomains}" + Style.RESET_ALL + Fore.GREEN + " subdomains were accessible" + Style.RESET_ALL)
+    else:
+        print(Fore.GREEN + "[+] Among them " + Fore.LIGHTCYAN_EX + Style.BRIGHT + f"{accessible_subdomains}" + Style.RESET_ALL + Fore.GREEN + " subdomains were accessible" + Style.RESET_ALL)
+    if len(ps_emails_return) == 0:
+        print(Fore.GREEN + "[+] In result, " + Fore.LIGHTRED_EX + Style.BRIGHT + f"{len(ps_emails_return)}" + Style.RESET_ALL + Fore.GREEN + " unique e-mail addresses were found" + Style.RESET_ALL)
+    else:
+        print(Fore.GREEN + "[+] In result, " + Fore.LIGHTCYAN_EX + Style.BRIGHT + f"{len(ps_emails_return)}" + Style.RESET_ALL + Fore.GREEN + " unique e-mail addresses were found" + Style.RESET_ALL)
+    if files_counter == 0:
+        print(Fore.GREEN + "[+] Also, " + Fore.LIGHTRED_EX + Style.BRIGHT + f"{files_counter}" + Style.RESET_ALL + Fore.GREEN + " files were extracted" + Style.RESET_ALL)
+    else:
+        print(Fore.GREEN + "[+] Also, " + Fore.LIGHTCYAN_EX + Style.BRIGHT + f"{files_counter}" + Style.RESET_ALL + Fore.GREEN + " files were extracted" + Style.RESET_ALL)
+    if cookies_counter == 0:
+        print(Fore.GREEN + "[+] Found " + Fore.LIGHTRED_EX + Style.BRIGHT + f"{cookies_counter}" + Style.RESET_ALL + Fore.GREEN + " cookies with values" + Style.RESET_ALL)
+    else:
+        print(Fore.GREEN + "[+] Found " + Fore.LIGHTCYAN_EX + Style.BRIGHT + f"{cookies_counter}" + Style.RESET_ALL + Fore.GREEN + " cookies with values" + Style.RESET_ALL)
+    if api_keys_counter == 0:
+        print(Fore.GREEN + "[+] Found " + Fore.LIGHTRED_EX + Style.BRIGHT + f"{api_keys_counter}" + Style.RESET_ALL + Fore.GREEN + " API keys" + Style.RESET_ALL)
+    else:
+        print(Fore.GREEN + "[+] Found " + Fore.LIGHTCYAN_EX + Style.BRIGHT + f"{api_keys_counter}" + Style.RESET_ALL + Fore.GREEN + " API keys" + Style.RESET_ALL)
+    if website_elements_counter == 0:
+        print(Fore.GREEN + "[+] Found " + Fore.LIGHTRED_EX + Style.BRIGHT + f"{website_elements_counter}" + Style.RESET_ALL + Fore.GREEN + " different web page elements" + Style.RESET_ALL)
+    else:
+        print(Fore.GREEN + "[+] Found " + Fore.LIGHTCYAN_EX + Style.BRIGHT + f"{website_elements_counter}" + Style.RESET_ALL + Fore.GREEN + " different web page elements" + Style.RESET_ALL)
+    if exposed_passwords_counter == 0:
+        print(Fore.GREEN + "[+] Found " + Fore.LIGHTRED_EX + Style.BRIGHT + f"{exposed_passwords_counter}" + Style.RESET_ALL + Fore.GREEN + " exposed passwords" + Style.RESET_ALL)
+    else:
+        print(Fore.GREEN + "[+] Found " + Fore.LIGHTCYAN_EX + Style.BRIGHT + f"{exposed_passwords_counter}" + Style.RESET_ALL + Fore.GREEN + " exposed passwords" + Style.RESET_ALL)
