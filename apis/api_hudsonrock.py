39020cbe64acb091736485754d49fc4772dc3679
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2025-02-06 23:49:15 +0300

    Changed output's visual design

--- apis/api_hudsonrock.py
+++ apis/api_hudsonrock.py
@@ -4,13 +4,10 @@ import re
 
 def hudsonrock_html_prep(formatted_output):
     formatted_output = re.sub(r'\x1b\[([0-9,A-Z]{1,2}(;[0-9]{1,2})?(;[0-9]{3})?)?[m|K]?', '', formatted_output)
-
-    start_marker = "===== HUDSONROCK API SCAN SUMMARY ====="
-    end_marker = "=== EMAIL DATA ==="
-
+    start_marker = "=== HUDSONROCK API REPORT ==="
+    end_marker = "[+] Email Data:"
     start_index = formatted_output.find(start_marker)
     end_index = formatted_output.find(end_marker)
-
     if start_index != -1 and end_index != -1:
         formatted_output = formatted_output[:start_index] + formatted_output[end_index:]
     return formatted_output
@@ -48,117 +45,94 @@ def api_hudsonrock_get(email=None, username=None, domain=None, ip=None):
 
     return results
 
+
 def api_hudsonrock_check(domain, ip, email, username):
     results = api_hudsonrock_get(email, username, domain, ip)
-    formatted_output = Fore.LIGHTMAGENTA_EX + "\n===== HUDSONROCK API SCAN SUMMARY =====\n\n" + Style.RESET_ALL
-    formatted_output += (Fore.LIGHTMAGENTA_EX + "=== PROVIDED DATA ===\n" + Style.RESET_ALL + Fore.GREEN + "\nDomain: " +
-            Fore.LIGHTCYAN_EX + f"{domain}\n" + Style.RESET_ALL +
-            Fore.GREEN + "IP: " + Fore.LIGHTCYAN_EX + f"{ip}\n" + Style.RESET_ALL +
-            Fore.GREEN + "E-mail: " + Fore.LIGHTCYAN_EX + f"{email}\n" + Style.RESET_ALL +
-            Fore.GREEN + "Username: " + Fore.LIGHTCYAN_EX + f"{username}\n" + Style.RESET_ALL
-    )
+    formatted_output = Fore.LIGHTBLUE_EX + "\n=== HUDSONROCK API REPORT ===\n" + Style.RESET_ALL
+    formatted_output += f"\n{Fore.LIGHTBLUE_EX}[+] Provided Data:{Style.RESET_ALL}\n"
+    formatted_output += f"{Fore.GREEN}Domain:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{domain}{Style.RESET_ALL}\n"
+    formatted_output += f"{Fore.GREEN}IP:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{ip}{Style.RESET_ALL}\n"
+    formatted_output += f"{Fore.GREEN}E-mail:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{email}{Style.RESET_ALL}\n"
+    formatted_output += f"{Fore.GREEN}Username:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{username}{Style.RESET_ALL}\n"
 
     def format_section(title, data):
         nonlocal formatted_output
-        formatted_output += Fore.LIGHTMAGENTA_EX + f"\n=== {title.upper()} ===\n" + Style.RESET_ALL
+        formatted_output += f"\n{Fore.LIGHTBLUE_EX}[+] {title}:{Style.RESET_ALL}\n"
         if 'error' in data:
-            formatted_output += Fore.RED + f"\nError appeared when trying to get results for {title} requests. Probably given data is incorrect.\n\n" + Style.RESET_ALL
+            formatted_output += f"{Fore.RED}Error appeared when trying to get results for {title} requests. Probably given data is incorrect.{Style.RESET_ALL}\n"
             return
 
         if title == 'Email Data':
-            formatted_output += Fore.GREEN + f"\n{data.get('message', 'No message available')}\n" + Style.RESET_ALL
+            formatted_output += f"{Fore.GREEN}Message:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{data.get('message', 'No message available')}{Style.RESET_ALL}\n"
             for i, stealer in enumerate(data.get('stealers', []), 1):
-                formatted_output += (
-                        Fore.GREEN + f"\n--- STEALER {i}" +
-                        Fore.GREEN + " ---\n" +
-                        "Computer Name: " + Fore.LIGHTCYAN_EX + f"{stealer.get('computer_name', 'Not Found')}" + Fore.GREEN + "\n" +
-                        "OS: " + Fore.LIGHTCYAN_EX + f"{stealer.get('operating_system', 'Not Found')}" + Fore.GREEN + "\n" +
-                        "Date Compromised: " + Fore.LIGHTCYAN_EX + f"{stealer.get('date_compromised', 'Not Found')}" + Fore.GREEN + "\n" +
-                        "Malware Path: " + Fore.LIGHTCYAN_EX + f"{stealer.get('malware_path', 'Not Found')}" + Fore.GREEN + "\n" +
-                        "IP: " + Fore.LIGHTCYAN_EX + f"{stealer.get('ip', 'Not Found')}" + Fore.GREEN + "\n" +
-                        "Top Passwords: " + Fore.LIGHTCYAN_EX + f"{', '.join(stealer.get('top_passwords', []))}" + Fore.GREEN + "\n" +
-                        "Top Logins: " + Fore.LIGHTCYAN_EX + f"{', '.join(stealer.get('top_logins', []))}" + "\n" +
-                        Style.RESET_ALL
-                )
+                formatted_output += f"\n{Fore.GREEN}--- STEALER {i} ---{Style.RESET_ALL}\n"
+                formatted_output += f"{Fore.GREEN}Computer Name:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{stealer.get('computer_name', 'Not Found')}{Style.RESET_ALL}\n"
+                formatted_output += f"{Fore.GREEN}OS:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{stealer.get('operating_system', 'Not Found')}{Style.RESET_ALL}\n"
+                formatted_output += f"{Fore.GREEN}Date Compromised:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{stealer.get('date_compromised', 'Not Found')}{Style.RESET_ALL}\n"
+                formatted_output += f"{Fore.GREEN}Malware Path:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{stealer.get('malware_path', 'Not Found')}{Style.RESET_ALL}\n"
+                formatted_output += f"{Fore.GREEN}IP:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{stealer.get('ip', 'Not Found')}{Style.RESET_ALL}\n"
+                formatted_output += f"{Fore.GREEN}Top Passwords:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{', '.join(stealer.get('top_passwords', []))}{Style.RESET_ALL}\n"
+                formatted_output += f"{Fore.GREEN}Top Logins:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{', '.join(stealer.get('top_logins', []))}{Style.RESET_ALL}\n"
+
         elif title == 'Username Data':
-            formatted_output += Fore.GREEN + f"\n{data.get('message', 'No message available')}\n" + Style.RESET_ALL
+            formatted_output += f"{Fore.GREEN}Message:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{data.get('message', 'No message available')}{Style.RESET_ALL}\n"
             for i, stealer in enumerate(data.get('stealers', []), 1):
-                formatted_output += (
-                        Fore.GREEN + f"\n--- STEALER {i}" +
-                        Fore.GREEN + " ---\n" +
-                        "Stealer Family: " + Fore.LIGHTCYAN_EX + f"{stealer.get('stealer_family', 'Not Found')}" + Fore.GREEN + "\n" +
-                        "Computer Name: " + Fore.LIGHTCYAN_EX + f"{stealer.get('computer_name', 'Not Found')}" + Fore.GREEN + "\n" +
-                        "OS: " + Fore.LIGHTCYAN_EX + f"{stealer.get('operating_system', 'Not Found')}" + Fore.GREEN + "\n" +
-                        "Date Compromised: " + Fore.LIGHTCYAN_EX + f"{stealer.get('date_compromised', 'Not Found')}" + Fore.GREEN + "\n" +
-                        "Malware Path: " + Fore.LIGHTCYAN_EX + f"{stealer.get('malware_path', 'Not Found')}" + Fore.GREEN + "\n" +
-                        "IP: " + Fore.LIGHTCYAN_EX + f"{stealer.get('ip', 'Not Found')}" + Fore.GREEN + "\n" +
-                        "Top Passwords: " + Fore.LIGHTCYAN_EX + f"{', '.join(stealer.get('top_passwords', []))}" + Fore.GREEN + "\n" +
-                        "Top Logins: " + Fore.LIGHTCYAN_EX + f"{', '.join(stealer.get('top_logins', []))}" + "\n" +
-                        Style.RESET_ALL
-                )
+                formatted_output += f"\n{Fore.GREEN}--- STEALER {i} ---{Style.RESET_ALL}\n"
+                formatted_output += f"{Fore.GREEN}Stealer Family:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{stealer.get('stealer_family', 'Not Found')}{Style.RESET_ALL}\n"
+                formatted_output += f"{Fore.GREEN}Computer Name:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{stealer.get('computer_name', 'Not Found')}{Style.RESET_ALL}\n"
+                formatted_output += f"{Fore.GREEN}OS:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{stealer.get('operating_system', 'Not Found')}{Style.RESET_ALL}\n"
+                formatted_output += f"{Fore.GREEN}Date Compromised:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{stealer.get('date_compromised', 'Not Found')}{Style.RESET_ALL}\n"
+                formatted_output += f"{Fore.GREEN}Malware Path:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{stealer.get('malware_path', 'Not Found')}{Style.RESET_ALL}\n"
+                formatted_output += f"{Fore.GREEN}IP:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{stealer.get('ip', 'Not Found')}{Style.RESET_ALL}\n"
+                formatted_output += f"{Fore.GREEN}Top Passwords:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{', '.join(stealer.get('top_passwords', []))}{Style.RESET_ALL}\n"
+                formatted_output += f"{Fore.GREEN}Top Logins:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{', '.join(stealer.get('top_logins', []))}{Style.RESET_ALL}\n"
 
         elif title == 'Domain Data':
-            formatted_output += (
-                    Fore.GREEN + "\nTotal Entries: " + Fore.LIGHTCYAN_EX + f"{data.get('total', 0)}" +
-                    Fore.GREEN + "\nTotal Stealers: " + Fore.LIGHTCYAN_EX + f"{data.get('totalStealers', 0)}" +
-                    "\n\n" + Style.RESET_ALL
-            )
-            formatted_output += Fore.GREEN + "Sample Employee URLs:\n" + Style.RESET_ALL
+            formatted_output += f"{Fore.GREEN}Total Entries:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{data.get('total', 0)}{Style.RESET_ALL}\n"
+            formatted_output += f"{Fore.GREEN}Total Stealers:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{data.get('totalStealers', 0)}{Style.RESET_ALL}\n"
+            formatted_output += f"\n{Fore.GREEN}Sample Employee URLs:{Style.RESET_ALL}\n"
             employee_urls = data.get('data', {}).get('employees_urls', [])
             if employee_urls:
                 for url_data in employee_urls[:10]:
-                    formatted_output += (
-                            Fore.GREEN + "Type: " + Fore.LIGHTCYAN_EX + f"{url_data.get('type', 'N/A')}" +
-                            Fore.GREEN + " | URL: " + Fore.LIGHTCYAN_EX + f"{url_data.get('url', 'N/A')}" +
-                            Fore.GREEN + " | Occurrence: " + Fore.LIGHTCYAN_EX + f"{url_data.get('occurrence', 'N/A')}" +
-                            Fore.GREEN + "\n" +
-                            Style.RESET_ALL
-                    )
+                    formatted_output += f"{Fore.GREEN}Type:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{url_data.get('type', 'N/A')}{Style.RESET_ALL}"
+                    formatted_output += f" {Fore.GREEN}| URL:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{url_data.get('url', 'N/A')}{Style.RESET_ALL}"
+                    formatted_output += f" {Fore.GREEN}| Occurrence:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{url_data.get('occurrence', 'N/A')}{Style.RESET_ALL}\n"
             else:
-                formatted_output += Fore.RED + "No employee URLs available.\n" + Style.RESET_ALL
+                formatted_output += f"{Fore.RED}No employee URLs available.{Style.RESET_ALL}\n"
+
         elif title == 'Attack Surface Data':
-            formatted_output += Fore.GREEN + f"\n{data.get('message', 'No message available')}\n" + Style.RESET_ALL
-            formatted_output += Fore.GREEN + "\nSample Employee URLs:\n" + Style.RESET_ALL
+            formatted_output += f"{Fore.GREEN}Message:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{data.get('message', 'No message available')}{Style.RESET_ALL}\n"
+            formatted_output += f"\n{Fore.GREEN}Sample Employee URLs:{Style.RESET_ALL}\n"
             employees = data.get('data', {}).get('employees_urls', [])
             if employees:
                 for url_data in employees[:10]:
-                    formatted_output += (
-                            Fore.GREEN + "Type: " + Fore.LIGHTCYAN_EX + f"{url_data.get('type', 'N/A')}" +
-                            Fore.GREEN + " | URL: " + Fore.LIGHTCYAN_EX + f"{url_data.get('url', 'N/A')}" +
-                            Fore.GREEN + " | Occurrence: " + Fore.LIGHTCYAN_EX + f"{url_data.get('occurrence', 'N/A')}" +
-                            Fore.GREEN + "\n" +
-                            Style.RESET_ALL
-                    )
+                    formatted_output += f"{Fore.GREEN}Type:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{url_data.get('type', 'N/A')}{Style.RESET_ALL}"
+                    formatted_output += f" {Fore.GREEN}| URL:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{url_data.get('url', 'N/A')}{Style.RESET_ALL}"
+                    formatted_output += f" {Fore.GREEN}| Occurrence:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{url_data.get('occurrence', 'N/A')}{Style.RESET_ALL}\n"
             else:
-                formatted_output += Fore.RED + "No employee URLs available.\n" + Style.RESET_ALL
-
-            formatted_output += Fore.GREEN + "\nSample Client URLs:\n" + Style.RESET_ALL
+                formatted_output += f"{Fore.RED}No employee URLs available.{Style.RESET_ALL}\n"
+            formatted_output += f"\n{Fore.GREEN}Sample Client URLs:{Style.RESET_ALL}\n"
             clients = data.get('data', {}).get('clients_urls', [])
             if clients:
                 for url_data in clients[:10]:
-                    formatted_output += (
-                            Fore.GREEN + "Type: " + Fore.LIGHTCYAN_EX + f"{url_data.get('type', 'N/A')}" +
-                            Fore.GREEN + " | URL: " + Fore.LIGHTCYAN_EX + f"{url_data.get('url', 'N/A')}" +
-                            Fore.GREEN + " | Occurrence: " + Fore.LIGHTCYAN_EX + f"{url_data.get('occurrence', 'N/A')}" +
-                            Fore.GREEN + "\n" + Style.RESET_ALL
-                    )
+                    formatted_output += f"{Fore.GREEN}Type:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{url_data.get('type', 'N/A')}{Style.RESET_ALL}"
+                    formatted_output += f" {Fore.GREEN}| URL:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{url_data.get('url', 'N/A')}{Style.RESET_ALL}"
+                    formatted_output += f" {Fore.GREEN}| Occurrence:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{url_data.get('occurrence', 'N/A')}{Style.RESET_ALL}\n"
             else:
-                formatted_output += "No client URLs available.\n"
+                formatted_output += f"{Fore.LIGHTCYAN_EX}No client URLs available.{Style.RESET_ALL}\n"
+
         elif title == 'IP Data':
-            formatted_output += Fore.GREEN + f"\n{data.get('message', 'No message available')}\n" + Style.RESET_ALL
+            formatted_output += f"{Fore.GREEN}Message:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{data.get('message', 'No message available')}{Style.RESET_ALL}\n"
             if data.get('stealers'):
                 for i, stealer in enumerate(data.get('stealers', []), 1):
-                    formatted_output += (
-                            Fore.GREEN + f"--- STEALER {i} ---\n" +
-                            "Computer Name: " + Fore.LIGHTCYAN_EX + f"{stealer.get('computer_name', 'Not Found')}" + Fore.GREEN + "\n" +
-                            "OS: " + Fore.LIGHTCYAN_EX + f"{stealer.get('operating_system', 'Not Found')}" + Fore.GREEN + "\n" +
-                            "Date Compromised: " + Fore.LIGHTCYAN_EX + f"{stealer.get('date_compromised', 'Not Found')}" + Fore.GREEN + "\n" +
-                            "Malware Path: " + Fore.LIGHTCYAN_EX + f"{stealer.get('malware_path', 'Not Found')}" + Fore.GREEN + "\n" +
-                            "IP: " + Fore.LIGHTCYAN_EX + f"{stealer.get('ip', 'Not Found')}" + Fore.GREEN + "\n" +
-                            "Top Passwords: " + Fore.LIGHTCYAN_EX + f"{', '.join(stealer.get('top_passwords', []))}" + Fore.GREEN + "\n" +
-                            "Top Logins: " + Fore.LIGHTCYAN_EX + f"{', '.join(stealer.get('top_logins', []))}" + "\n" +
-                            Style.RESET_ALL
-                    )
+                    formatted_output += f"\n{Fore.GREEN}--- STEALER {i} ---{Style.RESET_ALL}\n"
+                    formatted_output += f"{Fore.GREEN}Computer Name:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{stealer.get('computer_name', 'Not Found')}{Style.RESET_ALL}\n"
+                    formatted_output += f"{Fore.GREEN}OS:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{stealer.get('operating_system', 'Not Found')}{Style.RESET_ALL}\n"
+                    formatted_output += f"{Fore.GREEN}Date Compromised:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{stealer.get('date_compromised', 'Not Found')}{Style.RESET_ALL}\n"
+                    formatted_output += f"{Fore.GREEN}Malware Path:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{stealer.get('malware_path', 'Not Found')}{Style.RESET_ALL}\n"
+                    formatted_output += f"{Fore.GREEN}IP:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{stealer.get('ip', 'Not Found')}{Style.RESET_ALL}\n"
+                    formatted_output += f"{Fore.GREEN}Top Passwords:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{', '.join(stealer.get('top_passwords', []))}{Style.RESET_ALL}\n"
+                    formatted_output += f"{Fore.GREEN}Top Logins:{Style.RESET_ALL} {Fore.LIGHTCYAN_EX}{', '.join(stealer.get('top_logins', []))}{Style.RESET_ALL}\n"
         formatted_output += "\n"
 
     if 'email' in results:
