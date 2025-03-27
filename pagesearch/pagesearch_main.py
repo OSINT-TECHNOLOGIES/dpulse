617e24db3bc8dd367b19188863571ee9dbe3be12
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2025-03-27 03:29:47 +0300

    Removed redundant pagesearch_main module

--- pagesearch/pagesearch_main.py
+++
@@ -1,7 +0,0 @@
-from colorama import Fore, Style
-from pagesearch_parsers import subdomains_parser
-
-def normal_search(to_search_array, report_folder, keywords, keywords_flag):
-    print(Fore.GREEN + "Conducting PageSearch. Please, be patient, it may take a long time\n" + Style.RESET_ALL)
-    ps_emails_return, accessible_subdomains, emails_amount, files_counter, cookies_counter, api_keys_counter, website_elements_counter, exposed_passwords_counter, keywords_messages_list = subdomains_parser(to_search_array[0], report_folder, keywords, keywords_flag)
-    return ps_emails_return, accessible_subdomains, emails_amount, files_counter, cookies_counter, api_keys_counter, website_elements_counter, exposed_passwords_counter, keywords_messages_list
