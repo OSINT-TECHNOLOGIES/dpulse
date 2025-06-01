0f7b4576e2dba7f67b517285370606d65bb5e3a0
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2025-06-01 20:00:15 +0300

    Added config parameter to handle sitemap/robots file deletioin when creating HTML report

--- service/config_processing.py
+++ service/config_processing.py
@@ -27,7 +27,7 @@ def create_config():
     ]
 
     config = configparser.ConfigParser()
-    config['HTML_REPORTING'] = {'template': 'modern'}
+    config['HTML_REPORTING'] = {'template': 'modern', 'delete_txt_files': 'n'}
     config['LOGGING'] = {'log_level': 'info'}
     config['CLI VISUAL'] = {'preview_color': 'red', 'font': 'slant'}
     config['DORKING'] = {'dorking_delay (secs)': '2', 'delay_step': '5', 'full_path_to_browser': r'path\to\browser\for\dorking', 'browser_mode': 'nonheadless'}
@@ -61,6 +61,7 @@ def read_config():
     html_report_template = config.get('HTML_REPORTING', 'template')
     dorking_browser = config.get('DORKING', 'full_path_to_browser')
     dorking_browser_mode = config.get('DORKING', 'browser_mode')
+    delete_txt_files = config.get('HTML_REPORTING', 'delete_txt_files')
 
     config_values = {
         'logging_level': log_level,
@@ -76,7 +77,8 @@ def read_config():
         'wayback_requests_pause': wayback_requests_pause,
         'template': html_report_template,
         'dorking_browser': dorking_browser,
-        'dorking_browser_mode': dorking_browser_mode
+        'dorking_browser_mode': dorking_browser_mode,
+        'delete_txt_files': delete_txt_files
     }
 
     return config_values
