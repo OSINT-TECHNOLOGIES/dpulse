ee253fdb88b8e91e4534734586a61533bbc4463d
OSINT-TECHNOLOGIES <77023667+OSINT-TECHNOLOGIES@users.noreply.github.com>
2026-08-05 22:35:04 +0300

    Changed HudsonRock scan logic

--- python-backend/main.py
+++ python-backend/main.py
@@ -625,4 +625,4 @@ def utils_cve_info(cve: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8142)
